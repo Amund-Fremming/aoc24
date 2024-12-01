462970ec9450e8ed6eb8523358d9d77637bc2177
Amund-fremming <amund.fremming@gmail.com>
2024-12-01 20:25:33 +0100

    Finished day 1, probably bad solution

---
+++ puzzle1/Part1.cs
@@ -0,0 +1,37 @@
+﻿namespace puzzle1
+{
+    internal class Part1
+    {
+        public static void Solve()
+        {
+            List<int> left = [];
+            List<int> right = [];
+
+            using (StreamReader sr = new("PuzzleInput.txt"))
+            {
+                var line = sr.ReadLine();
+                while (line != null)
+                {
+                    var arr = line.Split(" ");
+                    left.Add(int.Parse(arr[0]));
+                    right.Add(int.Parse(arr[^1]));
+                    line = sr.ReadLine();
+                }
+
+                sr.Close();
+            }
+
+            left.Sort();
+            right.Sort();
+
+            var sum = 0;
+            for (int i = 0; i < left.Count; i++)
+            {
+                sum += Math.Abs(left[i] - right[i]);
+            }
+
+            Console.WriteLine($"Sum: {sum}");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ puzzle1/Part2.cs
@@ -0,0 +1,47 @@
+﻿namespace puzzle1
+{
+    internal class Part2
+    {
+        public static void Solve()
+        {
+            List<int> left = [];
+            Dictionary<int, int> rightCountTable = [];
+
+            using (StreamReader sr = new("PuzzleInput.txt"))
+            {
+                var line = sr.ReadLine();
+                while (line != null)
+                {
+                    var arr = line.Split(" ");
+                    left.Add(int.Parse(arr[0]));
+
+                    var rightValue = int.Parse(arr[^1]);
+                    var numberExists = rightCountTable.TryGetValue(rightValue, out var count);
+                    if (!numberExists)
+                    {
+                        rightCountTable[rightValue] = 1;
+                        line = sr.ReadLine();
+                        continue;
+                    }
+
+                    count++;
+                    rightCountTable[rightValue] = count;
+                    line = sr.ReadLine();
+                }
+
+                sr.Close();
+            }
+
+            var sum = 0;
+            foreach (var num in left)
+            {
+                var numberExist = rightCountTable.TryGetValue(num, out var count);
+                if (numberExist)
+                    sum += num * count;
+            }
+
+            Console.WriteLine($"Sum: {sum}");
+            Console.ReadKey();
+        }
+    }
+}
