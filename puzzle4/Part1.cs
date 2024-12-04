6c3dabd8e14bb98ac2319d59d50c797b0602b689
Amund-fremming <amund.fremming@gmail.com>
2024-12-04 16:57:36 +0100

    Refactored, works

--- puzzle4/Part1.cs
+++ puzzle4/Part1.cs
@@ -4,51 +4,47 @@
     {
         public static void Solve()
         {
-            List<List<char>> input = File.ReadAllLines("PuzzleInput.txt")
-                .Select(line =>
-                    line.ToCharArray()
-                        .ToList())
-                .ToList();
+            char[][] input = File.ReadAllLines("PuzzleInput.txt")
+                .Select(line => line.ToCharArray())
+                .ToArray();
 
-            var s = input[0];
             List<char> xmas = ['X', 'M', 'A', 'S'];
             var sum = 0;
-            for (int i = 0; i < input.Count; i++)
+            for (int i = 0; i < input.Length; i++)
             {
-                for (int j = 0; j < input[i].Count; j++)
+                for (int j = 0; j < input[i].Length; j++)
                 {
                     bool backwards = j - 3 >= 0;
                     bool upwards = i - 3 >= 0;
-                    bool forwards = j + 3 < input[i].Count;
-                    bool downwards = i + 3 < input.Count;
-
+                    bool forwards = j + 3 < input[i].Length;
+                    bool downwards = i + 3 < input.Length;
                     bool upLeft = backwards && upwards;
                     bool upRight = forwards && upwards;
                     bool downRight = forwards && downwards;
                     bool downLeft = backwards && downwards;
 
-                    if (forwards && TraverseForwards(input, xmas, i, j))
+                    if (forwards && Traverse(input, xmas, i, j, (0, 1)))
                         sum++;
 
-                    if (backwards && TraverseBackwards(input, xmas, i, j))
+                    if (backwards && Traverse(input, xmas, i, j, (0, -1)))
                         sum++;
 
-                    if (upwards && TraverseUpwards(input, xmas, i, j))
+                    if (upwards && Traverse(input, xmas, i, j, (-1, 0)))
                         sum++;
 
-                    if (downwards && TraverseDownwards(input, xmas, i, j))
+                    if (downwards && Traverse(input, xmas, i, j, (1, 0)))
                         sum++;
 
-                    if (upLeft && TraverseUpLeft(input, xmas, i, j))
+                    if (upLeft && Traverse(input, xmas, i, j, (-1, -1)))
                         sum++;
 
-                    if (upRight && TraverseUpRight(input, xmas, i, j))
+                    if (upRight && Traverse(input, xmas, i, j, (-1, 1)))
                         sum++;
 
-                    if (downRight && TraverseDownRight(input, xmas, i, j))
+                    if (downRight && Traverse(input, xmas, i, j, (1, 1)))
                         sum++;
 
-                    if (downLeft && TraverseDownLeft(input, xmas, i, j))
+                    if (downLeft && Traverse(input, xmas, i, j, (1, -1)))
                         sum++;
                 }
             }
@@ -56,98 +52,19 @@
             Console.WriteLine($"Sum : {sum}");
         }
 
-        public static bool TraverseForwards(List<List<char>> input, List<char> xmas, int i, int j)
+        public static bool Traverse(char[][] input, List<char> xmas, int i, int j, (int left, int right) operators)
         {
-            for (int x = 0; x < xmas.Count; x++)
+            for (int n = 0; n < xmas.Count; n++)
             {
-                if (input[i][j + x] != xmas[x])
-                {
-                    return false;
-                }
-            }
-            return true;
-        }
+                int GetVal(int val, int opr) => opr != 0
+                    ? val + (opr * n)
+                    : val;
 
-        public static bool TraverseBackwards(List<List<char>> input, List<char> xmas, int i, int j)
-        {
-            for (int x = 0; x < xmas.Count; x++)
-            {
-                if (input[i][j - x] != xmas[x])
-                {
-                    return false;
-                }
-            }
-            return true;
-        }
+                var x = GetVal(i, operators.left);
+                var y = GetVal(j, operators.right);
 
-        public static bool TraverseUpwards(List<List<char>> input, List<char> xmas, int i, int j)
-        {
-            for (int x = 0; x < xmas.Count; x++)
-            {
-                if (input[i - x][j] != xmas[x])
-                {
+                if (input[x][y] != xmas[n])
                     return false;
-                }
-            }
-            return true;
-        }
-
-        public static bool TraverseDownwards(List<List<char>> input, List<char> xmas, int i, int j)
-        {
-            for (int x = 0; x < xmas.Count; x++)
-            {
-                if (input[i + x][j] != xmas[x])
-                {
-                    return false;
-                }
-            }
-            return true;
-        }
-
-        public static bool TraverseUpLeft(List<List<char>> input, List<char> xmas, int i, int j)
-        {
-            for (int x = 0; x < xmas.Count; x++)
-            {
-                if (input[i - x][j - x] != xmas[x])
-                {
-                    return false;
-                }
-            }
-            return true;
-        }
-
-        public static bool TraverseUpRight(List<List<char>> input, List<char> xmas, int i, int j)
-        {
-            for (int x = 0; x < xmas.Count; x++)
-            {
-                if (input[i - x][j + x] != xmas[x])
-                {
-                    return false;
-                }
-            }
-            return true;
-        }
-
-        public static bool TraverseDownRight(List<List<char>> input, List<char> xmas, int i, int j)
-        {
-            for (int x = 0; x < xmas.Count; x++)
-            {
-                if (input[i + x][j + x] != xmas[x])
-                {
-                    return false;
-                }
-            }
-            return true;
-        }
-
-        public static bool TraverseDownLeft(List<List<char>> input, List<char> xmas, int i, int j)
-        {
-            for (int x = 0; x < xmas.Count; x++)
-            {
-                if (input[i + x][j - x] != xmas[x])
-                {
-                    return false;
-                }
             }
             return true;
         }
