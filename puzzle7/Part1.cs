6298bd0b18e6a6183f91450d65310a6f7b80c7ab
Amund-fremming <amund.fremming@gmail.com>
2024-12-10 16:02:49 +0100

    dont work

--- puzzle7/Part1.cs
+++ puzzle7/Part1.cs
@@ -4,7 +4,7 @@
     {
         public static void Solve()
         {
-            List<int> list = [1, 2, 3, 4];
+            List<int> list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
             List<string> operators = [];
             for (int i = 0; i < list.Count - 1; i++)
             {
@@ -12,10 +12,15 @@
             }
 
             var toFlip = 0;
-            var maxFlip = operators.Count - 1;
+            var maxFlip = operators.Count;
             var flipUpwards = true;
+            var combos = 0;
             for (int i = 0; i < Math.Pow(2, list.Count - 1); i++)
             {
+                combos++;
+                if (toFlip == -1)
+                    Console.WriteLine("Combos " + combos);
+
                 PrintTest(operators);
                 Console.WriteLine();
                 var (operatorsResult, nextFlipResult, maxFlipResult, flipUpwardsResult) = UpdateOperators(operators, toFlip, maxFlip, flipUpwards);
@@ -27,7 +32,7 @@
 
             //var input = GetPuzzleInput();
 
-            //var sum = 0;
+            //long sum = 0;
             //foreach (var line in input)
             //{
             //    var validOperations = ValidSum(line);
@@ -36,6 +41,7 @@
             //        sum += line[0];
             //    }
             //}
+            //Console.WriteLine($"Sum: {sum}");
         }
 
         public static void PrintTest(List<string> op)
@@ -44,7 +50,7 @@
                 Console.Write(i + " ");
         }
 
-        private static bool ValidSum(List<int> line)
+        private static bool ValidSum(List<long> line)
         {
             List<string> operators = [];
             for (int i = 0; i < line.Count - 2; i++)
@@ -52,22 +58,22 @@
                 operators.Add("+");
             }
 
-            bool flipUpwards = true;
             int toFlip = 0;
-            int maxFlip = operators.Count - 1;
+            bool flipUpwards = true;
+            int maxFlip = operators.Count;
             for (int i = 0; i < Math.Pow(2, line.Count - 2); i++)
             {
-                var iterationSum = 0;
+                var iterationSum = line.ElementAt(1);
                 var operatorIndex = 0;
                 for (var j = 2; j < line.Count; j++)
                 {
                     if (operators[operatorIndex] == "+")
                     {
-                        iterationSum += line[j - 1] + line[j];
+                        iterationSum += line[j];
                     }
                     if (operators[operatorIndex] == "*")
                     {
-                        iterationSum += line[j - 1] * line[j];
+                        iterationSum *= line[j];
                     }
                     operatorIndex++;
                 }
@@ -88,19 +94,21 @@
 
         private static (List<string> operatorsResult, int nextFlipResult, int maxFlipResult, bool flipUpwardsResult) UpdateOperators(List<string> operators, int toFlip, int maxFlip, bool flipUpwards)
         {
-            if (!flipUpwards && maxFlip > toFlip)
+            if (flipUpwards && maxFlip == 1 && toFlip == 1)
             {
-                return (operators, toFlip, maxFlip, flipUpwards);
-            }
-
-            if (flipUpwards && maxFlip < toFlip)
-            {
-                toFlip = 0;
-                maxFlip--;
+                flipUpwards = false;
+                maxFlip = -1;
+                toFlip = operators.Count - 1;
             }
 
             if (flipUpwards)
             {
+                if (maxFlip == toFlip)
+                {
+                    toFlip = 0;
+                    maxFlip--;
+                }
+
                 if (toFlip != 0)
                 {
                     operators[toFlip - 1] = FlipOperator(operators[toFlip - 1]);
@@ -108,21 +116,23 @@
 
                 operators[toFlip] = FlipOperator(operators[toFlip]);
                 toFlip++;
-                return (operators, toFlip, maxFlip, flipUpwards);
             }
 
-            if (toFlip != operators.Count - 1)
+            if (!flipUpwards)
             {
-                operators[toFlip + 1] = FlipOperator(operators[toFlip + 1]);
-            }
+                if (maxFlip == toFlip)
+                {
+                    toFlip = operators.Count - 1;
+                    maxFlip++;
+                }
 
-            operators[toFlip] = FlipOperator(operators[toFlip]);
-            toFlip--;
+                if (toFlip != operators.Count - 1)
+                {
+                    operators[toFlip + 1] = FlipOperator(operators[toFlip + 1]);
+                }
 
-            if (flipUpwards && toFlip == 0 && maxFlip == 0)
-            {
-                flipUpwards = false;
-                toFlip = operators.Count - 1;
+                operators[toFlip] = FlipOperator(operators[toFlip]);
+                toFlip--;
             }
 
             return (operators, toFlip, maxFlip, flipUpwards);
@@ -130,17 +140,17 @@
 
         public static string FlipOperator(string op) => op.Equals("+") ? "*" : "+";
 
-        private static List<List<int>> GetPuzzleInput()
+        private static List<List<long>> GetPuzzleInput()
         {
             string line;
-            List<List<int>> list = [];
+            List<List<long>> list = [];
             using var reader = new StreamReader("PuzzleInput.txt");
             while ((line = reader.ReadLine()!) != null)
             {
                 var split = line.Split(":");
-                var sum = int.Parse(split[0]);
+                var sum = long.Parse(split[0]);
                 var nums = split[1].Split(" ", StringSplitOptions.RemoveEmptyEntries)
-                    .Select(int.Parse)
+                    .Select(long.Parse)
                     .ToList();
 
                 nums.Insert(0, sum);
--- puzzle7/obj/Debug/net8.0/puzzle7.AssemblyInfo.cs
+++ puzzle7/obj/Debug/net8.0/puzzle7.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("puzzle7")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6c64f52e5863e0e6d144f67fce318ef25a44f95d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6ca94410dc80848df331c387c46ece57879d6de4")]
 [assembly: System.Reflection.AssemblyProductAttribute("puzzle7")]
 [assembly: System.Reflection.AssemblyTitleAttribute("puzzle7")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
